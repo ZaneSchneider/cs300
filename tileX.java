e81213388907db0e0b7686b30c587640cd5711f0
iang <gaul@pdx.edu>
2019-01-23 18:12:24 -0800

    typos

--- tileX.java
+++ tileX.java
@@ -1,13 +1,14 @@
 //sample tiles
+package com.company;
 import java.util.Random;
-public class Tile
+public class tilX//Tile
 {
     private int id;
     private String name;
     private int fare;
     private int chance_val;
 
-    Tile(){id = 0; name = ""; fare = 999; chance_val = new Random(); }
+    tileX(){id = 0; name = ""; fare = 999; chance_val = new Random(); }
 
     public void set_id(int id_in){ this.id = id_in;}
     public void set_name(String name_in){this.name = name_in;}
@@ -16,7 +17,7 @@ public class Tile
     public String get_fare(){return this.name;}
 
 
-public class bolder_tile extends Tile
+public class boulder_tile extends tileX
 {
     
     
