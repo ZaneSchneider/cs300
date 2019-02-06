51b32cac26a5e49f51de7ce7387d7d167912bb02
iang <gaul@pdx.edu>
2019-02-05 16:37:35 -0800

    uf

---
+++ Map.java
@@ -0,0 +1,37 @@
+/*----------------------------------------
+ * Class: Map
+ * This class will contain an array of tiles to be used for the game
+ * Frupal.  Class is not abstract, and needs to be customizable by 
+ * the player.
+ *
+ * ------------------------------------*/
+//package disabled so everyone doesnt have to set filepaths, set local
+//package company;
+
+public class Map {
+	
+	protected int xSize;
+	protected int ySize;
+	private Tile[][] area;
+
+	//Standard map constructor
+	public Map(){
+		this.area = new Tile[10][30];
+		this.xSize = 30;
+		this.ySize = 10;
+		for (int i = 0; i < ySize; i++){
+			for (int j = 0; j < xSize; j++){
+				this.area[i][j] = new Basic('O', "Plains", 2, 1);
+			}
+		}
+	}
+
+	public void printMap(){
+		for (int i = 0; i < this.ySize; i++){
+			for (int j = 0; j < this.xSize; j++){
+				System.out.print(area[i][j].get_id());
+			}
+			System.out.println();
+		}
+	}
+}	
--- Tile.java
+++ Tile.java
@@ -1,28 +1,86 @@
 //sample tiles
-package com.company;
+
+//package company;
+
 import java.util.Random;
-public class Tile
+
+public abstract class Tile
 {
-    private int id;         //tile id if needed
-    private String name;    // tile name
-    private int fare;       // energy cost for moving across tile, 0 is free, 999 is impassible 
-    private int chance_val; //int value for chance
+	protected int id;         //tile id if needed
+	protected int chance_val;  //for rand ops
+	protected String name;    // tile name
+	protected int fare;       // energy cost for moving across tile, 0 is free, 999 is impassible 
+	protected boolean seen;
+	
+	Random r = new Random();
+	int chance_range = 20;
 
-    Tile(){id = 0; name = ""; fare = 999; chance_val = new Random(); }  //default ctor
-    Tile(int id_in, String name, int fare_in){
-        id = id_in; name = name_in; fare = fare_in; chance_val = new Random(); }  //ctor takes all but chance val
-    Tile(int id_in, String name, int fare_in, int chance_in){
-        id = id_in; name = name_in; fare = fare_in; chance_val = chance_in; }  //ctor takes all values
+    Tile(){
+		id = 0; 
+		name = ""; 
+		fare = 999; 
+		chance_val = r.nextInt(chance_range);
+	 }  //default ctor
+    Tile(int id_in, String name_in, int fare_in){
+		id = id_in;
+		name = name_in; 
+		fare = fare_in; 
+		chance_val = r.nextInt(chance_range);
+	 }  //ctor takes all but chance val
+    Tile(int id_in, String name_in, int fare_in, int chance_in){
+		id = id_in;
+		name = name_in; 
+		fare = fare_in; 
+		chance_val = chance_in; 
+	}  //ctor takes all values
 
-    public void set_id(int id_in){ this.id = id_in;}
-    public void set_name(String name_in){this.name = name_in;}
-    public void set_fare(int fare_in){this.fare = fare_in;}
-    public String get_name(){return this.name;}
-    public String get_fare(){return this.name;}
+	public void set_id(int id_in){
+		this.id = id_in;
+	}
+	public void set_name(String name_in){
+		this.name = name_in;
+	}
+	public void set_fare(int fare_in){
+		this.fare = fare_in;
+	}
+	public void set_seen(){
+		this.seen = true;
+	}
+	public int get_id(){
+		return this.id;
+	}
+	public String get_name(){
+		return this.name;
+	}
+	public int get_fare(){
+		return this.fare;
+	}
+	public int get_chance(){
+		return chance_val;
+	}
+	public boolean get_seen(){
+		return this.seen;
+	}
+}
 
-
-public class boulder_tile extends Tile
+class Basic extends Tile 
 {
-    
-    
+	protected int income; //money gained from a natural tile
+	
+	//Constructor
+	Basic(char id, String name, int fare, int income){
+		this.id = id;
+		this.name = name;
+		this.fare = fare;
+		this.seen = false;
+		this.income = income;
+	}
+	//Setters
+	public void set_income(int income) {
+		this.income = income;
+	}
+	//Getters
+	public int get_income(){
+		return this.income;
+	}
 }
