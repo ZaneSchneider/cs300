21b63e3754bcd818f8783e095a61b7622f574bf7
igaul <40813772+igaul@users.noreply.github.com>
2019-02-11 18:53:38 -0800

    Update Main.java
    added color output strings

--- Main.java
+++ Main.java
@@ -15,6 +15,12 @@ import java.util.Scanner;
 
 public class Main{
 
+    public static final String NONE = "\033[0m"; //clears color
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN="\033[0;32m";
+    public static final String BRORNG="\033[0;33m";
+    public static final String BLUE="\033[0;34m";
+    public static final String YELLOW="\033[1;33m";
     private Map gameMap;
     private Hero hero;
 
